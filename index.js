dfd45b46a32064c2bc3386cd7445d4eba41bc164
ms-candy-codes <jordan.candy.code@gmail.com>
2025-01-28 15:31:41 +0200

    Add eraser eventlistener

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
 let divContainer = document.getElementsByClassName("divContainer")[0];
-let eraser = document.getElementsByClassName("eraser")[0];
+let eraser = document.getElementsByClassName("erase")[0];
 let num = 0;
+let isEraserActive = false;
 
-function createGrid(){
-let getNumber = Number(prompt("How big would you like your grid?"));
-num = getNumber;
+function createGrid() {
+    let getNumber = Number(prompt("How big would you like your grid?"));
+    num = getNumber;
 
-if(isNaN(num) || num < 0){
-    alert("Number is not acceptable");
-    return;
-}if(num > 50){
-    alert("Too big of a number");
-    return;
-}
-
-divContainer.innerHTML = "";
-
-for(let i = 0; i < num; i++){
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("rows");
-
-    for(let j = 0; j < num; j++){
-        const columnDiv = document.createElement("div");
-        columnDiv.classList.add("columns");;
-        rowDiv.appendChild(columnDiv);
-        columnDiv.addEventListener("mouseenter", ()=>{
-            columnDiv.classList.remove("columns");
-            columnDiv.classList.add("rows");
-        });
-
-        eraser.addEventListener("click", ()=>{
-            columnDiv.addEventListener("mouseenter", ()=>{
-                columnDiv.classList.remove("rows");
-                columnDiv.classList.add("columns");
-            })
-        });
+    if (isNaN(num) || num <= 0) {
+        alert("Number is not acceptable");
+        return;
+    }
+    if (num > 50) {
+        alert("Too big of a number");
+        return;
     }
 
-    divContainer.appendChild(rowDiv);
+    divContainer.innerHTML = "";
+
+    for (let i = 0; i < num; i++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+
+        for (let j = 0; j < num; j++) {
+            const columnDiv = document.createElement("div");
+            columnDiv.classList.add("cell");
+            rowDiv.appendChild(columnDiv);
+
+            columnDiv.addEventListener("mouseenter", () => {
+                if (!isEraserActive) {
+                    columnDiv.classList.add("hovered");
+                } else {
+                    columnDiv.classList.remove("hovered");
+                }
+            });
+        }
+
+        divContainer.appendChild(rowDiv);
+    }
 }
-}
+
+eraser.addEventListener("click", () => {
+    isEraserActive = !isEraserActive;
+    eraser.classList.toggle("active", isEraserActive);
+    if(isEraserActive === true){
+        eraser.textContent = "Draw";
+    }else{
+        eraser.textContent ="Erase";
+    }
+});
 
 createGrid();
-
-
-divContainer.addEventListener("mouseenter", ()=>{
-
-});
